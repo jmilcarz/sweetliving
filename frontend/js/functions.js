6ef277cbc335c1cc94bc3629e322ac761b5d0a39
kmilcarz <j.milcarz@yahoo.com>
2017-11-30 17:57:32 +0100

    navigation created

---
+++ frontend/js/functions.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+     var navStateOpen = false;
+     $("#toggleSiteMobileMenu").click(function() {
+          if (navStateOpen == false) {
+               $(".sl-container#mobileNav").slideDown(300);
+               navStateOpen = true;
+          }else {
+               $(".sl-container#mobileNav").slideUp(300);
+               navStateOpen = false;
+          }
+     });
+     if (screen.width > 980 && navStateOpen == true) {
+          $(".sl-container#mobileNav").css("display", "flex");
+     }
+
+     var searchOpen = false;
+     $("#toggleSearch").click(function() {
+          if (searchOpen == false) {
+               $(".nav-search-box").slideDown(400);
+               searchOpen = true;
+          }else {
+               $(".nav-search-box").slideUp(300);
+               searchOpen = false;
+          }
+     });
+
+     $("#toggleSearchMobile").click(function() {
+          if (searchOpen == false) {
+               $(".nav-search-box").slideDown(400);
+               searchOpen = true;
+          }else {
+               $(".nav-search-box").slideUp(300);
+               searchOpen = false;
+          }
+     });
+
+     $("#closeMenu").click(function() {
+          $(".nav-search-box").slideUp(300);
+     })
+});
